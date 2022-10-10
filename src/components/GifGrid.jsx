958eac455560562b371b59de545e3d42cd03d607
christian morales <christiandmoraless@gmail.com>
2022-10-09 20:16:17 -0400

    finish the gifApp

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,16 +1,25 @@
-import { getGif } from "../helpers/getGifs";
-
-
-
-
-
+import {GifItem} from './GifItem'
+import { useFetchGif } from "../hooks/useFetchGif";
 
 export const GifGrid = ({category}) => {
-
-getGif(category);   
+  const {images,isLoading} = useFetchGif(category)
   return (
     <>
         <h3>{category}</h3>
+        {
+          isLoading 
+          ? (<h2>Cargando....</h2>)
+          : null
+        }
+        <div className="card-grid">
+          {
+            images.map((image)=> (
+                <GifItem 
+                  key={image.id}
+                  {...image} />
+            ))
+          }
+        </div>
     </> 
   )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -16,7 +16,5 @@ export const getGif = async (category) => {
         title:img.title,
         url:img.images.downsized_medium.url
     }))
-
-    console.log(gifs);
     return gifs;
 }
